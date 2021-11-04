826ad3b605c45d4ed0c70ddddbc1c501849bed88
Alef-facode <alef.santos@facode.com.br>
2021-11-04 09:13:12 -0300

    introduzindo fontes

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
@@ -9,6 +9,7 @@ export class SidebarComponent implements OnInit {
 
   sidebarActive = false;
 
+  @Input() sidebarTitle: string = '';
   
   constructor() { }
   
